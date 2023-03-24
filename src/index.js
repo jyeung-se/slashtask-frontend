480a1dc16da8795ed09952408ab093349d4bf1e1
Jack Yeung <jyeung.se@gmail.com>
2023-03-24 15:53:10 -0400

    Installed disableReactDevTools for production mode

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
 import { Provider } from 'react-redux'
 import store from './store'
+import {disableReactDevTools} from '@fvilers/disable-react-devtools' 
 
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.render(
   <Provider store={store}>
